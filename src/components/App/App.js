c8bbf89c25ed0f7bf045dc35866b3aa9ee6ebb9b
Naomi Price <naomi.price71@gmail.com>
2019-09-27 15:32:29 -0500

    updated instructions from mary to clarify project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route,} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+      <Route path="/" component={Feeling}></Route>
+      <Route path="/" component={Feeling}></Route>
       </div>
+      </Router>
     );
   }
 }
