f33b96ab6e3746fa05fd8c4595fca975d2f08bce
Naomi Price <naomi.price71@gmail.com>
2019-09-28 15:32:27 -0500

    filled out Support component to send props to children and update redux with value

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import Select from '../Select/Select';
+import PageTitle from '../PageTitle/PageTitle';
+import {connect} from 'react-redux';
 
 class Support extends Component {
 
-    handleNext=()=>{
-
-        this.props.history.push('/comments');
+    pageAdvance = (answer)=>{
+        console.log(answer)
+        //update redux with answer value received from child
+        this.props.dispatch({type: 'SEND_FEEDBACK_VALUE', payload: {support: answer}})
+        //step to the next page
+        this.props.history.push('/comments')
     }
+    
     render(){
         return(
             <div>
-                <div>
-                    Do you feel Supported?
-                </div>
-                <button onClick={this.handleNext}>NEXT</button>
+                <PageTitle pageTitle={'How well are you being supported?'}/>
+                <Select pageAdvance={this.pageAdvance}
+                        description={`Support?: 1 for "I feel abandoned", 5 for "I feel supported!"`}/>
             </div>
         )
     }
 }
 
-export default Support;
+export default connect()(Support);
